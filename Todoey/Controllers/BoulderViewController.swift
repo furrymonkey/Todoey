7995aa02244e1590183cf317fdd775bfd9049faa
Apple <apple@jamies-macbook-pro.local>
2019-02-22 16:15:46 +0000

    Add boulder segue

--- Todoey/Controllers/BoulderViewController.swift
+++ Todoey/Controllers/BoulderViewController.swift
@@ -7,63 +7,152 @@
 //
 
 import UIKit
+import QuartzCore
 import SceneKit
 
-class BoulderViewController: UIViewController {
 
+class BoulderViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
         
-        let scnView = SCNView(frame: self.view.frame)
-        self.view.addSubview(scnView)
+        //create a new scene
+        let scene = SCNScene(named: "boulder.scnassets/Utopia.scn")!
+        let material = SCNMaterial()
         
-        let scene = SCNScene()
+        material.diffuse.contents = UIImage(named: "boulder.scnassets/Utopia_diffuse.png")
+        material.normal.contents = UIImage(named: "boulder.scnassets/Utopia_normal_map.png")
+        
+        //material.diffuse.contents = UIColor.red
+        
+        let boulder = scene.rootNode.childNode(withName: "utopia", recursively: true)
+        
+        boulder?.geometry?.materials = [material]
+        
+        // create and add a camera to the scene
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        scene.rootNode.addChildNode(cameraNode)
+        
+        // place the camera
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+        
+        // create and add a light to the scene
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light!.type = .omni
+        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
+        scene.rootNode.addChildNode(lightNode)
+        
+        // create and add an ambient light to the scene
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light!.type = .ambient
+        ambientLightNode.light!.color = UIColor.darkGray
+        scene.rootNode.addChildNode(ambientLightNode)
+        
+        // retrieve the ship node
+        //
+        
+        // animate the 3d object
+        boulder?.runAction(SCNAction.repeat(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 0.5), count: 4))
+        
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        // set the scene to the view
         scnView.scene = scene
         
+        // allows the user to manipulate the camera
+        scnView.allowsCameraControl = true
         
-        let cube = SCNBox(width: 1, height: 1, length: 1, chamferRadius: 0.02)
-        let cubeNode = SCNNode(geometry: cube)
+        // show statistics such as fps and timing information
+        scnView.showsStatistics = true
         
-        let redMaterial = SCNMaterial()
-        redMaterial.diffuse.contents = UIColor.red
-        cube.materials = [redMaterial]
+        // configure the view
+        scnView.backgroundColor = UIColor.black
         
-        let plane = SCNPlane(width: 50.0, height: 50.0)
-        let planeNode = SCNNode(geometry: plane)
-        planeNode.eulerAngles = SCNVector3(GLKMathDegreesToRadians(-90), 0, 0)
-        planeNode.position = SCNVector3(0, -0.5, 0)
-        
-        let greenMaterial = SCNMaterial()
-        greenMaterial.diffuse.contents = UIColor.green
-        plane.materials = [greenMaterial]
-        
-        let camera = SCNCamera()
-        let cameraNode = SCNNode()
-        
-        cameraNode.camera = camera
-        cameraNode.position = SCNVector3(x: -3.0,y: 3.0,z: 3.0)
-        
-        let light = SCNLight()
-        light.type = SCNLight.LightType.omni
-        let lightNode = SCNNode()
-        
-        lightNode.light = light
-        lightNode.position = SCNVector3(x: 1.5,y: 1.5,z: 1.5)
-        
-        scene.rootNode.addChildNode(cameraNode)
-        scene.rootNode.addChildNode(lightNode)
-        scene.rootNode.addChildNode(cubeNode)
-        scene.rootNode.addChildNode(planeNode)
-        
-        let constraint = SCNLookAtConstraint(target: cubeNode)
-        constraint.isGimbalLockEnabled = true
-        cameraNode.constraints = [constraint]
+        // add a tap gesture recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        scnView.addGestureRecognizer(tapGesture)
     }
+    
+    @objc
+    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        // check what nodes are tapped
+        let p = gestureRecognize.location(in: scnView)
+        let hitResults = scnView.hitTest(p, options: [:])
+        // check that we clicked on at least one object
+        if hitResults.count > 0 {
+            // retrieved the first clicked object
+            let result = hitResults[0]
+            
+            // get its material
+            let material = result.node.geometry!.firstMaterial!
+            
+            // highlight it
+            SCNTransaction.begin()
+            SCNTransaction.animationDuration = 0.5
+            
+            // on completion - unhighlight
+            SCNTransaction.completionBlock = {
+                SCNTransaction.begin()
+                SCNTransaction.animationDuration = 0.5
+                
+                material.emission.contents = UIColor.black
+                
+                SCNTransaction.commit()
+                
+            }
+            material.diffuse.contents = UIImage(named: "problems.scnassets/utopiaTraverse_diffuse.png")
+            
+            //material.emission.contents = UIColor.red
+            
+            
+            SCNTransaction.commit()
+        }
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+}
 
+
+
+extension SCNNode {
+    
+    convenience init(named name: String) {
+        self.init()
+        
+        guard let scene = SCNScene(named: name) else {
+            return
+        }
+        
+        for childNode in scene.rootNode.childNodes {
+            addChildNode(childNode)
+        }
+    }
+    
 }
